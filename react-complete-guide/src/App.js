8a89992fb4d499a45adeae9dc4561ea9a11fd64c
hjnp <hp_ctt@hotmail.com>
2018-05-04 17:15:36 +0100

    add better clean conditional dom toogle

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       { name: "b", age: 2 },
       { name: "c", age: 3 }
     ],
-    otherState: 'some other value',
+    otherState: "some other value",
     showPersons: false
   };
 
@@ -24,8 +24,7 @@ class App extends Component {
     });
   };
 
-
-  nameChangeHandler = (event) => {
+  nameChangeHandler = event => {
     this.setState({
       persons: [
         { name: "AA", age: 1 },
@@ -33,60 +32,56 @@ class App extends Component {
         { name: "CCC", age: 3 }
       ]
     });
-  }
+  };
 
   tooglePersonHandler = () => {
     this.setState({ showPersons: !this.state.showPersons });
-  }
+  };
 
   render() {
-
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '2px solid teal',
-      padding: '8px',
-      cursor: 'pointer',
-      color: 'teal'
-    }
+      backgroundColor: "white",
+      font: "inherit",
+      border: "2px solid teal",
+      padding: "8px",
+      cursor: "pointer",
+      color: "teal"
+    };
 
+    let persons;
+
+    if (this.state.showPersons) {
+      persons = 
+      <div>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          // two binding
+          // click={() => this.handleClick("Max!!")} //  more inefficient
+          click={this.handleClick.bind(this, "Max!!")} //  more efficient
+          changed={this.nameChangeHandler}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>;
+    }
 
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <button 
-        
-        style={style}
-        onClick={this.tooglePersonHandler}>Swith name</button>
-        
-       {
-         this.state.showPersons ? 
-         <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age}
-            />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              // two binding 
-              // click={() => this.handleClick("Max!!")} //  more inefficient
-              click={this.handleClick.bind(this, "Max!!")} //  more efficient
-              changed={this.nameChangeHandler}
-            />
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age}
-            > 
-            </Person>
-        </div> 
-        : 
-        <p>Hidden content</p>
-      }
+        <button style={style} onClick={this.tooglePersonHandler}>
+          Swith name
+        </button>
 
-
+        {persons}
       </div>
     );
   }
