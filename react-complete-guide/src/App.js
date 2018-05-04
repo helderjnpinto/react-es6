01cafbe210374238b851f69248cf11f029b0bc4d
hjnp <hp_ctt@hotmail.com>
2018-05-04 15:37:46 +0100

    add a new component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Person from './Person/Person';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Person name='Helder' age="29"/>
+        <Person name='Emanuel' age="40"/>
+        <Person name='Xavi' age="22"/>
       </div>
     );
   }
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// class Person extends Component {
+// }
+
+const person = (props) => {
+    return <p>
+        {/* I'm a Person with {Math.floor(Math.random() * 30)} years old! */}
+        I'm a {props.name} with {props.age} years old!
+    </p>
+}
+
+export default person;
