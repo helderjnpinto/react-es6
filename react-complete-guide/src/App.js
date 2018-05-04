0b09954cee81bb7ebbae77edc30efde626749556
hjnp <hp_ctt@hotmail.com>
2018-05-04 15:58:39 +0100

    add setState example

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,20 +5,30 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+  state = {
+      persons: [
+        {name: 'a', age: 1 },
+        {name: 'b', age: 2 },
+        {name: 'c', age: 3 },
+      ]
+  }
+
+  handleClick = () => {
+    this.setState({ persons: [
+      {name: 'AAA', age: 2 },
+      {name: 'BBB', age: 2 },
+      {name: 'CCC', age: 2 },
+    ] });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
-
-        <Person name='Helder' age="29"/>
-        <Person name='Emanuel' age="40">
-        <div>
-          Props children...
-        </div>
-        </Person>
-        <Person name='Xavi' age="22"/>
+        <button onClick={this.handleClick}>Swith name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
       </div>
     );
   }
