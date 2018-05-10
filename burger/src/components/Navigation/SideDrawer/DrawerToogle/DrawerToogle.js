00c7fab18bd1d42efb92dcc2bce83e79af56c487
hjnp <hp_ctt@hotmail.com>
2018-05-10 11:50:27 +0100

    add burger menu

--- burger/src/components/Navigation/SideDrawer/DrawerToogle/DrawerToogle.js
+++ burger/src/components/Navigation/SideDrawer/DrawerToogle/DrawerToogle.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
+import classes from "./DrawerToogle.css";
 
-const drawerToogle = (props) => (
-    <div onClick={props.clicked}> Menu </div>
+const drawerToogle = props => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
 );
 
-export default drawerToogle
+export default drawerToogle;
