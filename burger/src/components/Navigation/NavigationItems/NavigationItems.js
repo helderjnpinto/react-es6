4b064661f31336428f14b4bd00e39326d80528eb
hjnp <hp_ctt@hotmail.com>
2018-05-10 11:21:25 +0100

    woron sidedrawer and media queries

--- burger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import NavigationItem from './NavigationItem/NavigationItem';
 import classes from './NavigationItems.css';
-const navigationItems = (props) => (
-  <ul className={classes.NavigationItems}>
-    <NavigationItem link='/' active>Burger builder</NavigationItem>
-    <NavigationItem link='/'>Checkout</NavigationItem>
-  </ul>
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = () => (
+    <ul className={classes.NavigationItems}>
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
+    </ul>
 );
+
 export default navigationItems;
